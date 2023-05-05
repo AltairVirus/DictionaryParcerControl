f178bf1c3b4a7e23a94f9ac60f73f8d0c64fcecf
zkereitov <altairvirus@gmail.com>
2023-05-05 15:46:28 +0700

    well_running_2

--- main.py
+++ main.py
@@ -1,26 +1,25 @@
 import DBExtraction
 import ExcelWrite
 
-keys = DBExtraction.db_connect('ri.str_value', db='genres', look_for=True)  # Парсим из genres/rule_item список из КЛЮЧЕЙ
-values = DBExtraction.db_connect('rv.value', db='genres', look_for=True)  # Парсим из genres/rule_values список из ЗНАЧЕНИЙ
-attrbs = DBExtraction.db_connect(db='genres', look_for=False)  # Парсим из genres/rule_values список из КОДОВ АТРИБУТОВ
-opm_attrbs = DBExtraction.db_connect(db='opm')  # Парсим из opm/rule_values список из ВСЕХ АТРИБУТОВ + КОДЫ
 
-opm_attrbs_dict = {}  # Преобразуем список из ОРМ в словарь
-for a in opm_attrbs:
-    opm_attrbs_dict[a[0]] = a[1]
+def extract_values_from_collection(d, s=[]):
+    for i in d:
+        if type(i) in (set, tuple, dict, list):
+            extract_values_from_collection(i)
+        elif i not in s:
+            s.append(i)
+    return s
 
+
+keys = DBExtraction.db_connect('ri.str_value', db='genres', look_for=True)  # Парсим из genres/rule_item список КЛЮЧЕЙ
+values = DBExtraction.db_connect('rv.value', db='genres', look_for=True)  # Парсим из genres/rule_values список ЗНАЧЕНИЙ
+attrbs = DBExtraction.db_connect(db='genres', look_for=False)  # Парсим из genres/rule_values список КОДОВ АТРИБУТОВ
+opm_attrbs = DBExtraction.db_connect(db='opm')  # Парсим из opm/rule_values список ВСЕХ АТРИБУТОВ + КОДЫ
+
+opm_attrbs_dict = {a[0]: a[1] for a in opm_attrbs}  # Преобразуем список из ОРМ в словарь
 reference_uniq_cols_name = ExcelWrite.read_massive_from_excel()  # Cчитываем названия столбцов из Excel-файла
-print (values)
-db_keys_list = []
-for a in values:  # Формируем список значений из БД
-    for b in a:
-        for c in b:
-            for key, value in c.items():
-                if key not in db_keys_list:
-                    db_keys_list.append(key)
+db_keys_list = extract_values_from_collection(values)  # Формируем список значений из БД
 
-print(db_keys_list)
 full_reference_keys_list = []  # Формируем список всех необходимых Header'ов
 item_reference_keys_list = []  # Формируем список всех необходимых Header'ов для ЗНАЧЕНИЙ
 attrbs_reference_keys_list = []  # Формируем список всех необходимых Header'ов для АТРИБУТОВ
@@ -35,45 +34,41 @@ try:  # Сортируем в список значения согласно Hea
     values_new = []
     for a in values:
         for b in a:
-            help_list = []
+            hlp_list = []
             for m in item_reference_keys_list:
                 for c in b:
                     for d in c.keys():
                         if m == d:
-                            help_list.append(c.get(d))
-        values_new.append(help_list)
+                            hlp_list.append(c.get(d))
+        values_new.append(hlp_list)
 except:
     values_new = []
 
 keys_new = []
 for a in keys:
     for b in a:
-        help_list = []
+        hlp_list = []
         for c in b:
-            help_list.append(c)
-    keys_new.append(help_list)
+            hlp_list.append(c)
+    keys_new.append(hlp_list)
 
 attr_dict = {}  # Создаем словарь, ID атрибута : [последовтельность из значений атрибута]
 for a, a1 in enumerate(attrbs):
     if attrbs[a][1] in opm_attrbs_dict.keys():
         if opm_attrbs_dict.get(attrbs[a][1]) not in attr_dict.keys():
-            attr_dict[opm_attrbs_dict.get(attrbs[a][1])] = []  # opm_attrbs_dict.get(attrbs[a][1])
-            attr_dict[opm_attrbs_dict.get(attrbs[a][1])].append(attrbs[a][2])
+            attr_dict[opm_attrbs_dict.get(attrbs[a][1])] = [attrbs[a][2]]
         else:
             attr_dict[opm_attrbs_dict.get(attrbs[a][1])].append(attrbs[a][2])
     else:
         print(f"Атрибут с id: {attrbs[a][1]} не найден в базе ОРМ ")
 
-full_data = []
-# full_data.append(attrbs_reference_keys_list + item_reference_keys_list
-full_data.append(item_reference_keys_list)
+full_data = [item_reference_keys_list]
 
-if values_new != []:
+if values_new:
     for a_id, a in enumerate(keys_new):
-        # full_data.append(keys_new[a_id] + values_new[a_id])
         full_data.append(values_new[a_id])
 else:
-    full_data = keys_new
+    full_data += keys_new
 
 ExcelWrite.write_massive_to_excel(full_data, col=1, row=1)
 ExcelWrite.write_massive_to_excel(attr_dict, col=1, row=1)
